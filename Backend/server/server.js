f3253176bc1ed21b30f5516cbf3670ccb371eba2
adeel <adeelpv360@gmail.com>
2024-12-25 10:38:12 +0530

    implemente WebRtc for video and audio transmission

--- Backend/server/server.js
+++ Backend/server/server.js
@@ -7,6 +7,8 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
 
+const port = 3000
+
 var count = 0;
 var clients = {}
 
@@ -23,20 +25,34 @@ io.on("connection", (socket) => {
     console.log("New connection established", socket.id, count);
 
     // join a room
-    socket.on('joinRoom', (roomId) => {
-        socket.join(roomId);
-        console.log('Socket', socket.id, 'joined room: ', roomId);
-        clients[socket.id] = roomId;
-    })
+    socket.on('joinRoom', (username) => {
+        // socket.join(roomId);
+        // console.log('Socket', socket.id, 'joined room: ', roomId);
+        clients[username] = { username, id: socket.id };
+        io.emit("users", clients)
+    });
+
+    socket.on("offer", ({from, to, offer}) => {
+        console.log(`from: ${from} to: ${to} offer: ${offer}`)
+        io.to(clients[to].id).emit("offer", {from, to, offer});
+    });
+
+    socket.on("answer", ({from, to, answer}) => {
+        io.to(clients[from].id).emit("answer", answer)
+    });
+
+    socket.on("icecandidate", candidate => {
+        console.log(`candidate: ${candidate}`)
+        socket.broadcast.emit("icecandidate", candidate);
+    });
 
     socket.on("disconnect", () => {
         count -= 1;
         console.log("connection closed",socket.id, count);
         delete clients[socket.id]
-    })
+    });
 });
 
-const port = 3000
 server.listen(port, () => {
     console.log("server connected");
 });
--- Frontend/app.js
+++ Frontend/app.js
@@ -1,11 +1,12 @@
 
-let localStream = null;
-let remoteStream = null;
+let localStream;
+let remoteStream = document.querySelector("#remoteVideo");
+let CUsers;
 
 var socket = io();
-const roomId = `room-${Math.random().toString(36).substr(2, 9)}`;
+const username = `room-${Math.random().toString(36).substr(2, 9)}`;
 
-socket.emit('joinRoom', roomId);
+socket.emit('joinRoom', username);
 
 function init() {
     document.querySelector('#startBtn').addEventListener('click', openUserMedia);
@@ -13,21 +14,100 @@ function init() {
 }
 
 
+// Peer Connection
+const PeerConnection = (function() {
+    let peerConnection;
+
+    const createPeerConnection = () => {
+        const config = {
+            iceServers: [
+                {
+                    urls: 'stun:stun.l.google.com:19302'
+                }
+            ]
+        };
+
+        peerConnection = new RTCPeerConnection(config);
+
+        // add local stream to peer connection
+        localStream.getTracks().forEach(track => {
+            peerConnection.addTrack(track, localStream);
+        })
+        // listen to remote stream and add to peer connection
+        peerConnection.ontrack = function(event) {
+            remoteStream.style.transform = 'scaleX(-1)';
+            remoteStream.srcObject = event.streams[0];
+        }
+        // listen for ice candidate
+        peerConnection.onicecandidate = function(event) {
+            if(event.candidate){
+                socket.emit("icecandidate", event.candidate);
+            }
+        }
+
+        return peerConnection;
+    }
+
+    return {
+        getInstance: () => {
+            if(!peerConnection) {
+                peerConnection = createPeerConnection();
+            }
+            return peerConnection;
+        }
+    }
+})();
+
+socket.on("users", users => {
+    for(const user in users) {
+        if (user !== username){
+            CUsers = user
+        }
+    }
+})
+
+socket.on("offer", async ({from, to, offer}) => {
+    const pc = PeerConnection.getInstance();
+    // set remote description
+    await pc.setRemoteDescription(offer);
+    const answer = await pc.createAnswer();
+    await pc.setLocalDescription(answer);
+    socket.emit("answer", {from, to, answer: pc.localDescription});
+});
+
+socket.on("answer", async answer => {
+    const pc = PeerConnection.getInstance();
+    await pc.setRemoteDescription(answer)
+});
+
+socket.on("icecandidate", async candidate => {
+    console.log(`candidate: ${candidate}`);
+    const pc = PeerConnection.getInstance();
+    await pc.addIceCandidate(new RTCIceCandidate(candidate));
+});
+
 // Request access to the webcam
 async function openUserMedia(e) {
     const stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
     // Set the video source to the webcam stream
     document.querySelector('#localVideo').style.transform = 'scaleX(-1)';
-    document.querySelector('#localVideo').srcObject = stream
     localStream = stream;
+    document.querySelector('#localVideo').srcObject = stream;
 
-    socket.emit("localStream", localStream);
+    const pc = PeerConnection.getInstance();
+    const offer = await pc.createOffer();
+    console.log(`offer: ${offer}`);
+    await pc.setLocalDescription(offer);
 
-    socket.on("remoteStream", (remote) => {
-        remoteStream = remote;
-    })
+    socket.emit("offer", {from: username,to: CUsers, offer: pc.localDescription});
+
+    // socket.emit("localStream", localStream);
+
+    // socket.on("remoteStream", (remote) => {
+    //     remoteStream = remote;
+    // })
     
-    document.querySelector("#remoteVideo").srcObject = remoteStream;
+    // document.querySelector("#remoteVideo").srcObject = remoteStream;
 
     console.log('Stream: ', document.querySelector('#localVideo').srcObject);
     document.querySelector('#stopBtn').disabled = false;
@@ -36,6 +116,10 @@ async function openUserMedia(e) {
 }
 
 async function hangUp(e) {
+    const pc = PeerConnection.getInstance();
+    if(pc) {
+        pc.close();
+    }
     const tracks = document.querySelector('#localVideo').srcObject.getTracks();
     tracks.forEach(track => {
         track.stop();
